fa56d29370f303b8a98ad817a5f27f1c596c906b
Joost Boomkamp <joost.boomkamp@isatis-health.nl>
2018-12-04 09:13:26 +0100

    Day3

---
+++ Aoc2018.Test/Day03_1Tests.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace Aoc2018.Test
+{
+    class Day03_1Tests
+    {
+        private Day03 cut = new Day03();
+
+        private string[] _source = new[]
+        {
+            "#1 @ 1,3: 4x4",
+            "#2 @ 3,1: 4x4",
+            "#3 @ 5,5: 2x2"
+        };
+
+        [TestCase("#1 @ 1,3: 4x4", 1, 1, 3, 4, 4)]
+        [TestCase("#2 @ 3,1: 4x4", 2, 3, 1, 4, 4)]
+        [TestCase("#3 @ 5,5: 2x2", 3, 5, 5, 2, 2)]
+        public void TestParse(string src, int id, int x, int y, int width, int height)
+        {
+            var result = Claim.Parse(src);
+            Assert.AreEqual(result.Id, id);
+            Assert.AreEqual(result.Area.Left, x);
+            Assert.AreEqual(result.Area.Top, y);
+            Assert.AreEqual(result.Area.Right, x+width);
+            Assert.AreEqual(result.Area.Bottom, y+height);
+        }
+
+        [TestCase(1,1, true)]
+        [TestCase(1,2, true)]
+        [TestCase(2,1, true)]
+        [TestCase(1,3, false)]
+        [TestCase(2,3, false)]
+        [TestCase(3,1, false)]
+        [TestCase(3,2, false)]
+        [TestCase(3,3, true)]
+        public void ShouldOverlap(int id1, int id2, bool expected)
+        {
+            var claims = CreateClaims().ToDictionary(x => x.Id);
+
+            var actual = claims[id1].Covers(claims[id2]);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void Puzzle()
+        {
+            var result = cut.Puzzle(Source.Day3);
+            Assert.AreEqual(120408, result);
+        }
+
+        [Test]
+        public void Puzzle2()
+        {
+            var result = cut.Puzzle2(Source.Day3);
+            Assert.AreEqual(1276, result);
+        }
+
+        [Test]
+        public void Puzzle2TestCases()
+        {
+            var result = cut.Puzzle2(_source);
+            Assert.AreEqual(3, result);
+        }
+
+        private Claim[] CreateClaims()
+        {
+            return _source.Select(Claim.Parse).ToArray();
+        }
+    }
+}
---
+++ Aoc2018/Day03.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+namespace Aoc2018
+{
+    public class Day03
+    {
+        private Dictionary<int, Claim> Parse(string[] source)
+        {
+            return source.Select(Claim.Parse).ToDictionary(x => x.Id);
+        }
+
+        public int Puzzle(string[] source)
+        {
+            var claimsById = Parse(source);
+            var claims = claimsById.Values.ToArray();
+
+            var doubleCovered = 0;
+
+            var texture = new Rectangle(1, 1, claims.Max(x => x.Area.Right), claims.Max(x => x.Area.Bottom));
+            var covercount = new int[texture.Width,texture.Height];
+
+            foreach (var claim in claims)
+            {
+                for (var x = claim.Area.Left; x < claim.Area.Right; x++)
+                {
+                    for (var y = claim.Area.Top; y < claim.Area.Bottom; y++)
+                    {
+                        covercount[x, y]++;
+                    }
+                }
+            }
+
+            foreach (var z in covercount)
+            {
+                if (z > 1)
+                    doubleCovered++;
+            }
+
+            return doubleCovered;
+        }
+
+        public int Puzzle2(string[] source)
+        {
+            var coversAnything = false;
+            var claimsById = Parse(source);
+            foreach (var claim in claimsById)
+            {
+                foreach (var other in claimsById)
+                {
+                    if (other.Key == claim.Key) 
+                        continue; // don't check vs self
+
+                    if (claim.Value.Covers(other.Value))
+                    {
+                        coversAnything = true;
+                    }
+                }
+
+                if (!coversAnything)
+                    return claim.Key;
+                coversAnything = false; // reset for next iteration
+            }
+
+            return 0;
+        }
+    }
+
+    public class Claim
+    {
+        public Rectangle Area { get; set; }
+        public int Id { get; set; }
+
+        public static Claim Parse(string source)
+        {
+            var clean = source.Replace(":", "").Replace("#", "").Replace(",", " ").Replace("x", " ").Replace("@", "");
+            var subs = clean.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            var ints = subs.Select(int.Parse).ToArray();
+            return new Claim
+            {
+                Id = ints[0],
+                Area = new Rectangle(ints[1], ints[2], ints[3], ints[4])
+            };
+        }
+
+        public bool Covers(int x, int y)
+        {
+            return Area.Left < x && Area.Right > x &&
+                   Area.Top < y && Area.Bottom > y;
+        }
+
+        public bool Covers(Claim other)
+        {
+            return other.Area.Left < Area.Right &&
+                   other.Area.Right > Area.Left &&
+                   other.Area.Top < Area.Bottom &&
+                   other.Area.Bottom > Area.Top;
+        }
+    }
+}
