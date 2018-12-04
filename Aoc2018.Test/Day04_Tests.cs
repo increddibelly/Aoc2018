023b9e06045df8e6dffc67bd06f7b51181426f2c
Joost Boomkamp <joost.boomkamp@isatis-health.nl>
2018-12-04 17:09:35 +0100

    Day4

---
+++ Aoc2018.Test/Day04_Tests.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Linq;
+using NUnit.Framework;
+
+namespace Aoc2018.Test
+{
+    [TestFixture]
+    class Day04_Tests
+    {
+        private static string[] _source = new[]
+        {
+            "[1518-11-01 00:00] Guard #10 begins shift",
+            "[1518-11-01 00:05] falls asleep",
+            "[1518-11-01 00:25] wakes up",
+            "[1518-11-01 00:30] falls asleep",
+            "[1518-11-01 00:55] wakes up",
+            "[1518-11-01 23:58] Guard #99 begins shift",
+            "[1518-11-02 00:40] falls asleep",
+            "[1518-11-02 00:50] wakes up",
+            "[1518-11-03 00:05] Guard #10 begins shift",
+            "[1518-11-03 00:24] falls asleep",
+            "[1518-11-03 00:29] wakes up",
+            "[1518-11-04 00:02] Guard #99 begins shift",
+            "[1518-11-04 00:36] falls asleep",
+            "[1518-11-04 00:46] wakes up",
+            "[1518-11-05 00:03] Guard #99 begins shift",
+            "[1518-11-05 00:45] falls asleep",
+            "[1518-11-05 00:55] wakes up",
+        };
+
+        private Day04 _cut = new Day04();
+
+        [Test]
+        public void ShouldParse()
+        {
+            var result = _cut.Parse(_source);
+            
+            Assert.AreEqual(17, result.Length);
+            foreach (var x in result)
+            {
+                Assert.That(x.Timestamp.Year == 2018);
+                Assert.That(x.Timestamp.Month == 11);
+            }
+
+            Assert.IsTrue(result[1].FallAsleep);
+            Assert.IsFalse(result[1].WakeUp);
+            
+            Assert.IsTrue(result[2].WakeUp);
+            Assert.IsFalse(result[2].FallAsleep);
+
+            Assert.That(result[5].Timestamp == new DateTime(2018, 11, 1, 23, 58, 0));
+
+            Assert.That(result[0].Guard == 10);
+            Assert.That(result[1].Guard == 10);
+            Assert.That(result[2].Guard == 10);
+            Assert.That(result[3].Guard == 10);
+            Assert.That(result[4].Guard == 10);
+            Assert.That(result[5].Guard == 99);
+        }
+    }
+}
---
+++ Aoc2018/Day04.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+
+namespace Aoc2018
+{
+    public class Day04
+    {
+        public Log[] Parse(string[] input)
+        {
+            var result = input.Select(Log.Parse).OrderBy(x=> x.Timestamp).ToArray();
+            var currentGuard = result[0].Guard;
+            foreach (var item in result)
+            {
+                if (item.Guard > 0)
+                {
+                    currentGuard = item.Guard;
+                }
+                else
+                {
+                    item.Guard = currentGuard;
+                }
+            }
+            return result;
+        }
+    }
+
+    public class Log
+    {
+        public DateTime Timestamp { get;set; }
+        public int Guard { get; set; } = -1;
+        public bool WakeUp = false;
+        public bool FallAsleep = false;
+
+        public Log(string input)
+        {
+            Timestamp = ParseTime(input);
+        }
+
+        public static Log Parse(string input)
+        {
+            if (input.Contains("Guard")) return ParseGuard(input);
+            if (input.Contains("wakes")) return ParseWakesUp(input);
+            if (input.Contains("falls")) return ParseFallsAsleep(input);
+            return null;
+        }
+
+        private static DateTime ParseTime(string input)
+        {
+            input = input.Replace("1518-", "2018-");
+            return DateTime.ParseExact(input.Substring(1, 16), "yyyy-MM-dd HH:mm", CultureInfo.CurrentCulture);
+        }
+
+        private static Log ParseFallsAsleep(string input)
+        {
+            return new Log(input) {FallAsleep = true};
+        }
+
+        private static Log ParseWakesUp(string input)
+        {
+            return new Log(input) {WakeUp = true};
+        }
+
+        private static Log ParseGuard(string input)
+        {
+            var guardId = input.Split(' ')[3].Replace("#", "");
+            return new Log (input)
+            {
+                Guard = int.Parse(guardId)
+            };
+        }
+    }
+}
