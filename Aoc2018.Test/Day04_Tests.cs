20df71fcc34386d461350d84ee65a1cd031db785
Joost Boomkamp <joost.boomkamp@isatis-health.nl>
2018-12-07 12:41:42 +0100

    Day4

--- Aoc2018.Test/Day04_Tests.cs
+++ Aoc2018.Test/Day04_Tests.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using NUnit.Framework;
 
 namespace Aoc2018.Test
@@ -7,7 +9,7 @@ namespace Aoc2018.Test
     [TestFixture]
     class Day04_Tests
     {
-        private static string[] _source = new[]
+        private static readonly string[] _source = new[]
         {
             "[1518-11-01 00:00] Guard #10 begins shift",
             "[1518-11-01 00:05] falls asleep",
@@ -34,28 +36,131 @@ namespace Aoc2018.Test
         public void ShouldParse()
         {
             var result = _cut.Parse(_source);
+            var shifts = result.Split();
             
-            Assert.AreEqual(17, result.Length);
+            Assert.AreEqual(17, result.Count);
             foreach (var x in result)
             {
                 Assert.That(x.Timestamp.Year == 2018);
                 Assert.That(x.Timestamp.Month == 11);
             }
 
-            Assert.IsTrue(result[1].FallAsleep);
-            Assert.IsFalse(result[1].WakeUp);
+            Assert.IsTrue(result[1].Type == Event.EventType.FallAsleep);
             
-            Assert.IsTrue(result[2].WakeUp);
-            Assert.IsFalse(result[2].FallAsleep);
+            Assert.IsTrue(result[2].Type == Event.EventType.WakeUp);
 
             Assert.That(result[5].Timestamp == new DateTime(2018, 11, 1, 23, 58, 0));
 
             Assert.That(result[0].Guard == 10);
+            Assert.That(result[0].Type == Event.EventType.SetGuard);
             Assert.That(result[1].Guard == 10);
             Assert.That(result[2].Guard == 10);
             Assert.That(result[3].Guard == 10);
             Assert.That(result[4].Guard == 10);
             Assert.That(result[5].Guard == 99);
+
+            Assert.That(shifts.Count == 4);
+            Assert.That(shifts[0].SleepDuration == 45);
+        }
+
+        [Test]
+        public void Puzzle()
+        {
+            var shifts = _cut.ToShifts(_cut.Parse(Source.Day4));
+            var sleepsumPerGuard = shifts.ToDictionary(x => x.Key, x => x.Value.Sum(y => y.SleepDuration));
+            var sleepiest = sleepsumPerGuard.OrderByDescending(x => x.Value).First();
+
+            Assert.AreEqual(2663, sleepiest.Key);
+            Assert.AreEqual(522, sleepsumPerGuard[sleepiest.Key]);
+
+            var shiftsForSleepiestGuard = shifts[2663];
+
+            var output = new Dictionary<int, int>();
+            for (var i = 0; i <= 59; i++) output.Add(i, 0); // set all minutes to 0
+
+            foreach (var shift in shiftsForSleepiestGuard)
+            {
+                for (var minute = 0; minute <= 59; minute++)
+                {
+                    output[minute] = output[minute] + shift.CountSleepsAt(minute);
+                }
+            }
+
+            var max = output.OrderByDescending(x => x.Value).First();
+            Assert.AreEqual(45, max.Key);
+            Assert.AreEqual(15, max.Value);
+
+            Assert.AreEqual(119835, sleepiest.Key * max.Key);
+        }
+
+        [Test]
+        public void Puzzle2()
+        {
+            var shifts = _cut.ToShifts(_cut.Parse(Source.Day4));
+
+            var sleepsPerMinutePerGuard = new Dictionary<int, Dictionary<int, int>>();
+            foreach (var shiftsForGuard in shifts)
+            {
+                var guard = shiftsForGuard.Key;
+                sleepsPerMinutePerGuard.Add(guard, new Dictionary<int, int>());
+
+                foreach (var shift in shifts[guard])
+                {
+                    for (var minute = 0; minute <= 59; minute++)
+                    {
+                        if (!sleepsPerMinutePerGuard[guard].ContainsKey(minute))
+                        {
+                            sleepsPerMinutePerGuard[guard].Add(minute, 0);
+                        }
+                        sleepsPerMinutePerGuard[guard][minute] += shift.SleepsAt(minute) ? 1 : 0;
+                    }
+                }
+            }
+            
+            var sleepiestMinutePerGuard = new Dictionary<int, int>();
+            foreach (var item in sleepsPerMinutePerGuard)
+            {
+                var guard = item.Key;
+                var sleepsPerMinute = item.Value;
+                var maxSleepingAtMinute = sleepsPerMinute.OrderByDescending(x => x.Value).First();
+                sleepiestMinutePerGuard.Add(guard, maxSleepingAtMinute.Key);
+            }
+
+            var agg = sleepiestMinutePerGuard.OrderByDescending(x => x.Value).ToArray();
+            var output = agg.First();
+            Assert.AreEqual(433, output.Key);
+            Assert.AreEqual(49, output.Value);
+        }
+
+        [Test]
+        public void Puzzle2_2()
+        {
+            var shiftsPerGuard = _cut.ToShifts(_cut.Parse(Source.Day4));
+
+            var theMinute = -1;
+            var theGuard = 0;
+            var theCount = 0;
+
+            foreach (var shift in shiftsPerGuard)
+            {
+                var guard = shift.Key;
+                var record = _cut.TotalSleepsPerMinute(shift.Value);
+
+                foreach (var minute in record)
+                {
+                    if (minute.Value > theCount)
+                    {
+                        theMinute = minute.Key;
+                        theCount = minute.Value;
+                        theGuard = guard;
+                    }
+                }
+            }
+
+            Assert.AreEqual(20, theCount);
+            Assert.AreEqual(509, theGuard);
+            Assert.AreEqual(25, theMinute);
+            Assert.AreEqual(12725, theMinute * theGuard);
         }
     }
 }
--- Aoc2018/Day04.cs
+++ Aoc2018/Day04.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
 
@@ -6,38 +8,129 @@ namespace Aoc2018
 {
     public class Day04
     {
-        public Log[] Parse(string[] input)
+        public EventList Parse(string[] input)
         {
-            var result = input.Select(Log.Parse).OrderBy(x=> x.Timestamp).ToArray();
-            var currentGuard = result[0].Guard;
-            foreach (var item in result)
-            {
-                if (item.Guard > 0)
-                {
-                    currentGuard = item.Guard;
-                }
-                else
-                {
-                    item.Guard = currentGuard;
-                }
-            }
+            var result = new EventList(input);
+            
             return result;
         }
-    }
 
-    public class Log
+        public Dictionary<int, List<Shift>> ToShifts(EventList list)
+        {
+            return list.Split().GroupBy(x => x.Guard).ToDictionary(x => x.Key, x => x.ToList());
+        }
+
+        public Dictionary<int, int> TotalSleepsPerMinute(IEnumerable<Shift> shifts)
+        {
+            var output = Shift.BlankHourSheet();
+            for (var minute = 0; minute <= 59; minute++)
+            {
+                foreach (var shift in shifts)
+                {
+                    output[minute] += shift.SleepsPerMinute[minute];
+                }
+            }
+
+            return output;
+        }
+    }
+    
+    public class Shift
     {
+        public int Guard { get; protected set; }
+        public Event[] Events { get; protected set; }
+        public int SleepDuration { get; protected set; }
+        public List<Period> Sleeps { get; protected set; }
+        public Dictionary<int, int> SleepsPerMinute { get; set; } = BlankHourSheet();
+
+        public Shift(Event[] events)
+        {
+            Events = events.OrderBy(x => x.Guard).ThenBy(x => x.Timestamp).ToArray();
+            Sleeps = new List<Period>();
+            Setup();
+        }
+        public static Dictionary<int, int> BlankHourSheet()
+        {
+            var d = new Dictionary<int, int>(60);
+            for (var minute = 0; minute <= 59; minute++)
+            {
+                d.Add(minute, 0);
+            }
+
+            return d;
+        }
+
+        public string ToString()
+        {
+            return
+                $"Guard# {Guard}, {Events?.Length ?? 0} Events, {Sleeps?.Count ?? 0} Sleeps for {SleepDuration} minutes";
+        }
+
+        private void Setup()
+        {
+            Guard = Events[0].Guard;
+            SleepDuration = SleepDurationMinutes();
+        }
+
+        private int SleepDurationMinutes()
+        {
+            var sleepStart = DateTime.MinValue;
+            var sleepEnd = DateTime.MaxValue;
+            foreach (var t in Events)
+            {
+                switch (t.Type)
+                {
+                    case Event.EventType.SetGuard: break;
+                    case Event.EventType.FallAsleep:
+                        sleepStart = t.Timestamp;
+                        break;
+                    case Event.EventType.WakeUp:
+                        sleepEnd = t.Timestamp;
+                        var period = new Period {Start = sleepStart, End = sleepEnd};
+                        Sleeps.Add(period);
+                        for (var minute = period.Start.Minute; minute < period.End.Minute; minute++)
+                        {
+                            SleepsPerMinute[minute]++;
+                        }
+                        break;
+                }
+            }
+
+            return Sleeps.Sum(x => x.Duration);
+        }
+
+        public int CountSleepsAt(int minute)
+        {
+            return Sleeps.Count(x => x.Start.TimeOfDay.Minutes <= minute && 
+                                     x.End.TimeOfDay.Minutes   >  minute);
+        }
+
+        public bool SleepsAt(int minute)
+        {
+            return Sleeps.Any(x => x.Start.TimeOfDay.Minutes <= minute && 
+                                   x.End.TimeOfDay.Minutes   >  minute);
+        }
+    }
+    
+    public class Event
+    {
+        public enum EventType : byte
+        {
+            SetGuard,
+            WakeUp,
+            FallAsleep
+        };
+
         public DateTime Timestamp { get;set; }
         public int Guard { get; set; } = -1;
-        public bool WakeUp = false;
-        public bool FallAsleep = false;
+        public EventType Type { get; set; }
 
-        public Log(string input)
+        public Event(string input)
         {
             Timestamp = ParseTime(input);
         }
 
-        public static Log Parse(string input)
+        public static Event Parse(string input)
         {
             if (input.Contains("Guard")) return ParseGuard(input);
             if (input.Contains("wakes")) return ParseWakesUp(input);
@@ -51,23 +144,144 @@ namespace Aoc2018
             return DateTime.ParseExact(input.Substring(1, 16), "yyyy-MM-dd HH:mm", CultureInfo.CurrentCulture);
         }
 
-        private static Log ParseFallsAsleep(string input)
+        private static Event ParseFallsAsleep(string input)
         {
-            return new Log(input) {FallAsleep = true};
+            return new Event(input) {Type = EventType.FallAsleep};
         }
 
-        private static Log ParseWakesUp(string input)
+        private static Event ParseWakesUp(string input)
         {
-            return new Log(input) {WakeUp = true};
+            return new Event(input) {Type = EventType.WakeUp};
         }
 
-        private static Log ParseGuard(string input)
+        private static Event ParseGuard(string input)
         {
             var guardId = input.Split(' ')[3].Replace("#", "");
-            return new Log (input)
+            return new Event (input)
             {
+                Type = EventType.SetGuard,
                 Guard = int.Parse(guardId)
             };
+        }
+    }
+
+    public class EventList
+    {
+        private readonly List<Event> _events;
+
+        public EventList(string[] source)
+        {
+            _events = source.Select(Event.Parse).OrderBy(x => x.Timestamp).ToList();
+
+            var currentGuard = _events[0].Guard;
+            foreach (var item in _events)
+            {
+                if (item.Guard > 0)
+                {
+                    currentGuard = item.Guard;
+                }
+                else
+                {
+                    item.Guard = currentGuard;
+                }
+            }
+        }
+
+        public List<Shift> Split()
+        {
+            var tempShift = new List<Event>();
+            var shifts = new List<Shift>();
+            foreach (var item in _events)
+            {
+                if (!tempShift.Any() || item.Type != Event.EventType.SetGuard)
+                    tempShift.Add(item);
+                else
+                {
+                    shifts.Add(new Shift(tempShift.ToArray()));
+                    tempShift.Clear();
+                }
+            }
+
+            return shifts.OrderBy(x => x.Guard).ToList();
+        }
+
+        private Shift FindShift(DateTime timestamp)
+        {
+            var middle = EventAtTime(timestamp);
+            var start = FindShiftStart(middle);
+            var end = FindShiftEnd(middle);
+
+            return new Shift(_events
+                .Where(x => x.Timestamp >= start.Timestamp)
+                .Where(x => x.Timestamp <= end.Timestamp)
+                .OrderBy(x => x.Timestamp)
+                .ToArray());
+        }
+
+        /// <summary>
+        /// first event (set guard) for this guard
+        /// </summary>
+        /// <returns></returns>
+        public Event FindShiftStart(Event someEventInShift)
+        {
+            if (someEventInShift.Type == Event.EventType.SetGuard) return someEventInShift;
+            return _events
+                .Where(x => x.Type == Event.EventType.SetGuard)
+                .Where(x => x.Timestamp < someEventInShift.Timestamp)
+                .OrderByDescending(x => x.Timestamp)
+                .Last();
+        }
+
+        /// <summary>
+        /// last event for this guard
+        /// </summary>
+        /// <param name="someEventInShift"></param>
+        /// <returns></returns>
+        public Event FindShiftEnd(Event someEventInShift)
+        {
+            var next = _events.First(x => x.Timestamp > someEventInShift.Timestamp && x.Type != Event.EventType.SetGuard);
+            return _events.Last(x => x.Timestamp < next.Timestamp);
+        }
+
+        public Event EventAtTime(DateTime timestamp)
+        {
+            Event current = null;
+
+            foreach (var evt in _events)
+            {
+                if (evt.Timestamp < timestamp)
+                    current = evt;
+                if (evt.Timestamp >= timestamp)
+                {
+                    break;
+                }
+            }
+
+            return current;
+        }
+
+        public Event this[int index] => _events[index];
+
+        public IEnumerator<Event> GetEnumerator()
+        {
+            return _events.GetEnumerator();
+        }
+
+        public int Count => _events.Count;
+    }
+
+    public class Period
+    {
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+        public int Duration => (End-Start).Minutes;
+    }
+
+    public static class Extensions
+    {
+        public static int Characteristic(this double source)
+        {
+            return Convert.ToInt32(Math.Floor(source));
         }
     }
 }
