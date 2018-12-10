55bfc18357b63dd452b950eb773933bddfab4bfb
Joost Boomkamp <joost.boomkamp@isatis-health.nl>
2018-12-10 17:03:19 +0100

    Day7

---
+++ Aoc2018.Test/Day07_Tests.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace Aoc2018.Test
+{
+    class Day07_Tests
+    {
+        private Day07 _cut = new Day07();
+
+        private readonly string[] _testcase = new[]
+        {
+            "Step C must be finished before step A can begin.",
+            "Step C must be finished before step F can begin.",
+            "Step A must be finished before step B can begin.",
+            "Step A must be finished before step D can begin.",
+            "Step B must be finished before step E can begin.",
+            "Step D must be finished before step E can begin.",
+            "Step F must be finished before step E can begin."
+        };
+
+        [Test]
+        public void ShouldParseTestCase()
+        {
+            var result = Instructions.Parse(_testcase);
+            var x = result.instructionList;
+
+            Assert.IsFalse(x["C"].Dependencies.Any());
+            Assert.IsTrue(x["A"].Dependencies.Any(d => d.Id == "C"));
+            Assert.IsTrue(x["F"].Dependencies.Any(d => d.Id == "C"));
+            Assert.IsTrue(x["B"].Dependencies.Any(d => d.Id == "A"));
+            Assert.IsTrue(x["D"].Dependencies.Any(d => d.Id == "A"));
+            Assert.IsTrue(x["E"].Dependencies.Any(d => d.Id == "B"));
+            Assert.IsTrue(x["E"].Dependencies.Any(d => d.Id == "D"));
+            Assert.IsTrue(x["E"].Dependencies.Any(d => d.Id == "F"));
+
+            Assert.AreEqual(1, x["F"].Dependencies.Count);
+            Assert.AreEqual(3, x["E"].Dependencies.Count);
+            Assert.AreEqual(1, x["D"].Dependencies.Count);
+            Assert.AreEqual(1, x["B"].Dependencies.Count);
+            Assert.AreEqual(1, x["A"].Dependencies.Count);
+            Assert.AreEqual(0, x["C"].Dependencies.Count);
+        }
+
+        
+        [Test]
+        public void ShouldRunTestCaseManualOrder()
+        {
+            var result = Instructions.Parse(_testcase);
+            var x = result.instructionList;
+            var reporter = new Reporter();
+            var a = x["A"];
+            var b = x["B"];
+            var c = x["C"];
+            var d = x["D"];
+            var e = x["E"];
+            var f = x["F"];
+
+            Assert.IsTrue(c.CanRun);
+            Assert.IsFalse(a.CanRun);
+            Assert.IsFalse(b.CanRun);
+            Assert.IsFalse(d.CanRun);
+            Assert.IsFalse(e.CanRun);
+            Assert.IsFalse(f.CanRun);
+            // run step 1
+            Assert.AreEqual(Status.New, c.Status);
+            c.Run(reporter);
+            Assert.AreEqual(Status.Completed, c.Status);
+            // check end state step 1
+            Assert.IsTrue(a.CanRun);
+            Assert.IsFalse(b.CanRun);
+            Assert.IsFalse(d.CanRun);
+            Assert.IsFalse(e.CanRun);
+            Assert.IsTrue(f.CanRun);
+            
+            // run step 2
+            Assert.AreEqual(Status.New, a.Status);
+            a.Run(reporter);
+            Assert.AreEqual(Status.Completed, a.Status);
+            // check end state step 2
+            Assert.IsTrue(b.CanRun);
+            Assert.IsTrue(d.CanRun);
+            Assert.IsFalse(e.CanRun);
+            Assert.IsTrue(f.CanRun);
+
+            // run step 3
+            Assert.AreEqual(Status.New, b.Status);
+            b.Run(reporter);
+            Assert.AreEqual(Status.Completed, b.Status);
+            // check end state step 3
+            Assert.IsTrue(d.CanRun);
+            Assert.IsFalse(e.CanRun);
+            Assert.IsTrue(f.CanRun);
+            
+            // run step 4
+            Assert.AreEqual(Status.New, d.Status);
+            d.Run(reporter);
+            Assert.AreEqual(Status.Completed, d.Status);
+            // check end state step 4
+            Assert.IsFalse(e.CanRun);
+            Assert.IsTrue(f.CanRun);
+
+            // run step 5
+            Assert.AreEqual(Status.New, f.Status);
+            f.Run(reporter);
+            Assert.AreEqual(Status.Completed, f.Status);
+            // check end state step 5
+            Assert.IsTrue(e.CanRun);
+
+            // run step 6
+            Assert.AreEqual(Status.New, e.Status);
+            e.Run(reporter);
+            Assert.AreEqual(Status.Completed, e.Status);
+
+            Assert.IsTrue(x.All(instr => instr.Value.Status == Status.Completed));
+
+            var output = reporter.ToString();
+            Assert.AreEqual("CABDFE", output);
+        }
+
+
+        [Test]
+        public void ShouldRunTestCase()
+        {
+            var result = _cut.Puzzle(_testcase);
+            Assert.AreEqual("CABDFE", result);
+        }
+
+        [Test]
+        public void CanRunTest()
+        {
+            var a = new Instruction("A");
+            var b = new Instruction("B");
+            var c = new Instruction("C");
+            var rep = new Reporter();
+
+            c.Dependencies.Add(b);
+            b.Dependencies.Add(a);
+            Assert.IsFalse(b.CanRun);
+            Assert.IsFalse(c.CanRun);
+
+            Assert.IsTrue(a.CanRun);
+            a.Run(rep);
+            Assert.IsTrue(b.CanRun);
+            Assert.IsFalse(c.CanRun);
+            b.Run(rep);
+            Assert.IsTrue(c.CanRun);
+            c.Run(rep);
+
+            var result = rep.ToString();
+            Assert.AreEqual("ABC", result);
+        }
+        
+        [Test]
+        public void CanNotRunTest()
+        {
+            var a = new Instruction("A");
+            var b = new Instruction("B"){ Status = Status.Blocked};
+            var c = new Instruction("C");
+            var d = new Instruction("D");
+            
+            c.Dependencies.Add(b);
+            b.Dependencies.Add(a);
+            d.Dependencies.Add(b);
+            d.Dependencies.Add(a);
+            Assert.IsTrue(a.CanRun);
+            Assert.IsFalse(b.CanRun);
+            Assert.IsFalse(c.CanRun);
+            Assert.IsFalse(d.CanRun);
+        }
+
+        [Test]
+        public void RunPuzzle()
+        {
+            var result = _cut.Puzzle(Source.Day7);
+            Assert.AreEqual("BITRAQVSGUWKXYHMZPOCDLJNFE", result);
+        }
+
+    }
+}
---
+++ Aoc2018/Day07.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Aoc2018
+{
+    public class Day07
+    {
+        public string Puzzle(string[] input)
+        {
+            var rep = new Reporter();
+            var program = Instructions.Parse(input);
+
+            var next = program.Next;
+            do
+            {
+                next?.Run(rep);
+                next = program.Next;
+            } while (next != null);
+
+            return rep.ToString();
+        }
+    }
+
+    public class Instructions
+    {
+        public Dictionary<string, Instruction> instructionList { get; protected set; }
+
+        public Instruction Next
+        {
+            get { return 
+                instructionList
+                    .OrderBy(x => x.Key)
+                    .Where(x => x.Value.Status != Status.Completed)
+                    .FirstOrDefault(x => x.Value.CanRun)
+                    .Value;
+            }
+        }
+
+        private Instructions()
+        {
+
+        }
+
+        public static Instructions Parse(string[] input)
+        {
+            var ordered = input.OrderBy(x => x).ToArray();
+
+            var instructions = new Instructions();
+            instructions.instructionList = new Dictionary<string, Instruction>();
+
+            foreach (var line in ordered)
+            {
+                var dependencyKey = ParseDependencyKey(line);
+                var instrKey = ParseInstructionKey(line);
+                
+                if (!instructions.instructionList.TryGetValue(dependencyKey, out var dependency))
+                {
+                    dependency = new Instruction(dependencyKey);
+                    instructions.instructionList.Add(dependencyKey, dependency);
+                }
+                
+                if (!instructions.instructionList.TryGetValue(instrKey, out var instruction))
+                {
+                    instruction = new Instruction(instrKey);
+                    instructions.instructionList.Add(instrKey, instruction);
+                }
+
+                instruction.Dependencies.Add(dependency);
+            }
+
+            return instructions;
+        }
+
+        private static string ParseDependencyKey(string line)
+        {
+            // Step C must be finished before step A can begin.
+            // should return C
+            var items = line.Split(" ");
+            return items[1];
+        }
+        
+        private static string ParseInstructionKey(string line)
+        {
+            // Step C must be finished before step A can begin.
+            // should return A
+            var items = line.Split(" ");
+            return items[7];
+        }
+    }
+
+    public class Instruction
+    {
+        public string Id { get; protected set; }
+        public Status Status { get; set; } = Status.New;
+        public List<Instruction> Dependencies { get; set; } = new List<Instruction>();
+        public bool CanRun => 
+            Status != Status.Blocked 
+            && 
+            Dependencies.All(x => x.Status == Status.Completed);
+
+        public void Run(Reporter reporter)
+        {
+            if (!CanRun) return;
+            reporter = reporter ?? new Reporter();
+
+            var deps = Dependencies
+                .Where(x => x.Status != Status.Completed)
+                .OrderBy(x => x.Id);
+            foreach (var dep in deps)
+            {
+                dep.Run(reporter);
+            }
+
+            reporter.Append(Id);
+            Status = Status.Completed;
+        }
+
+        public Instruction(string key)
+        {
+            Id = key;
+        }
+    }
+
+    public enum Status : byte
+    {
+        New = 0,
+        Blocked=5,
+        Completed = 10
+    }
+
+    public class Reporter
+    {
+        private readonly StringBuilder Report = new StringBuilder();
+
+        public void Append(string x)
+        {
+            Report.Append(x);
+        }
+
+        public new string ToString()
+        {
+            return Report.ToString();
+        }
+    }
+}
