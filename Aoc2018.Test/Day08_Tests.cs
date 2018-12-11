32253bb0245a1f5f36bad065a5a430970c28872b
Joost Boomkamp <joost.boomkamp@isatis-health.nl>
2018-12-11 16:35:44 +0100

    Day 8

---
+++ Aoc2018.Test/Day08_Tests.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace Aoc2018.Test
+{
+    class Day08_Tests
+    {
+        Day08 _cut;
+        private readonly Queue<int> _testCase = "2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2".ToQueue();
+
+        [SetUp]
+        public void Setup()
+        {
+            _cut = new Day08();
+            Node.Reset();
+        }
+
+        [Test]
+        public void ShouldParse()
+        {
+            var root = Node.Parse(_testCase);
+            Assert.AreEqual(2, root.ChildCount);
+            Assert.AreEqual(3, root.MetaDataCount);
+            Assert.AreEqual(1, root.MetaData[0]);
+            Assert.AreEqual(1, root.MetaData[1]);
+            Assert.AreEqual(2, root.MetaData[2]);
+            Assert.AreEqual(1, root.Id);
+            
+            Assert.AreEqual(0, root.Children[0].ChildCount);
+            Assert.AreEqual(3, root.Children[0].MetaDataCount);
+            Assert.AreEqual(10, root.Children[0].MetaData[0]);
+            Assert.AreEqual(11, root.Children[0].MetaData[1]);
+            Assert.AreEqual(12, root.Children[0].MetaData[2]);
+            Assert.AreEqual(2, root.Children[0].Id);
+
+            Assert.AreEqual(1, root.Children[1].ChildCount);
+            Assert.AreEqual(1, root.Children[1].MetaDataCount);
+            Assert.AreEqual(2, root.Children[1].MetaData[0]);
+            Assert.AreEqual(3, root.Children[1].Id);
+
+            Assert.AreEqual(0, root.Children[1].Children[0].ChildCount);
+            Assert.AreEqual(1, root.Children[1].Children[0].MetaDataCount);
+            Assert.AreEqual(99, root.Children[1].Children[0].MetaData[0]);
+            Assert.AreEqual(4, root.Children[1].Children[0].Id);
+
+            Assert.AreEqual(138, root.Sum());
+        }
+
+        [Test]
+        public void ShouldSolveTestcase()
+        {
+            var root = _cut.Puzzle(_testCase);
+            Assert.AreEqual(138, root);
+        }
+
+        [Test]
+        public void ShouldFind()
+        {
+            var root = Node.Parse(_testCase);
+            var result1 = root.Find(1);
+            var result2 = root.Find(2);
+            var result3 = root.Find(3);
+            var result4 = root.Find(4);
+            var result5 = root.Find(5);
+
+            Assert.AreEqual(1, result1.Id);
+            Assert.AreEqual(2, result2.Id);
+            Assert.AreEqual(3, result3.Id);
+            Assert.AreEqual(4, result4.Id);
+            Assert.AreEqual(null, result5);
+        }
+
+        [Test]
+        public void Puzzle()
+        {
+            var result = _cut.Puzzle(Source.Day8);
+            Assert.AreEqual(40309, result);
+        }
+
+        [Test]
+        public void ShouldGetValueTestcase()
+        {
+            var root = Node.Parse(_testCase);
+            var value = root.Value();
+            Assert.AreEqual(66, value);
+        }
+
+        [Test]
+        public void Puzzle2()
+        {
+            var result = _cut.Puzzle2(Source.Day8);
+            Assert.AreEqual(28779, result);
+        }
+    }
+}
---
+++ Aoc2018/Day08.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Aoc2018
+{
+    public class Day08
+    {
+        public int Puzzle(Queue<int> source)
+        {
+            var root = Node.Parse(source);
+            return root.Sum();
+        }
+
+        public int Puzzle2(Queue<int> source)
+        {
+            var root = Node.Parse(source);
+            return root.Value();
+        }
+    }
+
+    public class Node
+    {
+        public int Id { get; protected set; }
+        public int ChildCount { get; protected set; }
+        public List<Node> Children { get; protected set; }
+        public int MetaDataCount { get; protected set; }
+        public int[] MetaData { get; protected set; }
+
+        public int Sum()
+        {
+            return Children.Sum(x => x.Sum()) + MetaData.Sum();
+        }
+
+        private static int _nextId = 0;
+        private static int IdGenerator()
+        {
+            _nextId++;
+            return _nextId;
+        }
+
+        public Node Find(int id)
+        {
+            if (this.Id == id) return this ;
+
+            return Children.Select(x => x.Find(id)).FirstOrDefault(x => x != null);
+        }
+
+        public int Value()
+        {
+            if (ChildCount == 0)
+            {
+                return MetaData.Sum();
+            }
+
+            var sum = 0;
+            var dataSet = 
+                MetaData.GroupBy(x => x)
+                        .ToDictionary(x => x.Key, x => x.Count());
+
+            foreach (var set in dataSet)
+            {
+                var index = set.Key - 1;
+
+                if (ChildCount <= index || ChildCount < 1)
+                    continue;
+
+                // calculate once
+                var childValue = Children[index].Value();
+                sum += set.Value * childValue;
+            }
+
+            return sum;
+        }
+
+        public static Node Parse(Queue<int> input)
+        {
+            // 2 3  0 3  10 11 12  1 1  0 1 99  2  1 1 2
+            // A---------------------------------------A
+            //      B-----------B  C------------C
+            //                          D---D
+
+            var childCount = input.Dequeue(); // 2
+            var metaDataCount = input.Dequeue(); // 3
+            
+            // store what we have.
+            var node = new Node
+            {
+                Id = IdGenerator(),
+                ChildCount = childCount,
+                MetaDataCount = metaDataCount,
+                Children = new List<Node>(childCount)
+            };
+
+            // parse children before parsing this.metadata
+            for (var child = 0; child < childCount; child++)
+            {
+                var childNode = Parse(input);
+                node.Children.Add(childNode);
+            }
+
+            var metaData = new List<int>(2*metaDataCount);
+            for (var i=0; i<metaDataCount; i++)
+            {
+                metaData.Add(input.Dequeue());
+            }
+
+            node.MetaData = metaData.ToArray();
+
+            return node;
+
+        }
+
+        public static void Reset()
+        {
+            _nextId = 0;
+        }
+    }
+
+    public static class extensions
+    {
+        public static Queue<int> ToQueue(this string source)
+        {
+            return new Queue<int>(source.Split(" ").Select(int.Parse));
+        }
+    }
+}
